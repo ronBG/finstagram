125dd54ed45d912c54d057b0ca6e453f68b2bcf1
ronBG Bolanos <ronaldgbolanos@gmail.com>
2023-03-17 22:20:27 +0000

    working workflow and statement in ruby

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,5 @@
-
-def humanized_time_ago(minute_num)
-    if minute_num >= 60
-      "#{time_ago_in_minutes / 60} hours ago"
-    else
-      "#{time_ago_in_minutes} minutes ago"
-    end
-end
-  
-  get '/' do
-    username = "sharky_j"
+get '/' do
+  username = "sharky_j"
   avatar_url = "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg"
   photo_url = "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg"
   time_ago_in_minutes = 15
@@ -17,6 +8,10 @@ end
   comments = [
     "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
   ]
-
-  humanized_time_ago(time_ago_in_minutes)
+  
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
+  end
 end
