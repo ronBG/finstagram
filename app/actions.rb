4b725caff5fbd8a575d633863a2ccb89e207f785
ronBG Bolanos <ronaldgbolanos@gmail.com>
2023-03-17 16:48:15 +0000

    added font styling from fonts

---
+++ app/actions.rb
@@ -0,0 +1,22 @@
+
+def humanized_time_ago(minute_num)
+    if minute_num >= 60
+      "#{time_ago_in_minutes / 60} hours ago"
+    else
+      "#{time_ago_in_minutes} minutes ago"
+    end
+end
+  
+  get '/' do
+    username = "sharky_j"
+  avatar_url = "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg"
+  photo_url = "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg"
+  time_ago_in_minutes = 15
+  like_count = 0
+  comment_count = 1
+  comments = [
+    "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+  ]
+
+  humanized_time_ago(time_ago_in_minutes)
+end
