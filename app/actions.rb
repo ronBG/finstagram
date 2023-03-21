a75bb17b8949e1ec0cb416db1a26397fc53dcef3
ronBG Bolanos <ronaldgbolanos@gmail.com>
2023-03-21 21:38:56 +0000

    Creating Models useing ActiveRecord to set up our DB

--- app/actions.rb
+++ app/actions.rb
@@ -8,7 +8,7 @@ end
 
 get '/' do
   
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg",
     photo_url: "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg",
@@ -21,7 +21,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "https://live.staticflickr.com/65535/52358421348_f34c7996b1.jpg",
     photo_url: "https://live.staticflickr.com/65535/52357237337_1cc718f6a7_4k.jpg",
@@ -34,7 +34,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "https://live.staticflickr.com/65535/52358415933_0a0e6bc35f_3k.jpg",
     photo_url: "https://live.staticflickr.com/65535/52358494794_f88b160d15_4k.jpg",
@@ -47,5 +47,7 @@ get '/' do
     }]
   }
 
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
 end
