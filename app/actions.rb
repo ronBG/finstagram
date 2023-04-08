32941708139a171cb001816957fd4df98c5699f8
ronBG Bolanos <ronaldgbolanos@gmail.com>
2023-04-08 22:47:56 +0000

    inplement Create comment: Creat like: Delete Like

--- app/actions.rb
+++ app/actions.rb
@@ -77,3 +77,26 @@ post '/finstagram_posts' do
     erb(:"finstagram_posts/new")
   end
 end
+
+post '/comments' do 
+text = params[:text]
+finstagram_post_id = params[:finstagram_post_id]
+comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: user_id })
+comment.save
+redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-    
     belongs_to :user
-    belongs_to :finstagram_post
+    belongs_to :finstagram_post 
+    validate_presence_of :text, :user, :finstagram_post
 
 end
